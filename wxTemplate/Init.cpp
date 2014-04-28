ff51a55d1fd2640df171d648c83bd922c0724f72
zzlee <zzlee.tw@gmail.com>
2014-04-28 15:05:03 +0800

    add wxtemplate

---
+++ wxTemplate/Init.cpp
@@ -0,0 +1,124 @@
+#include "stdafx.h"
+
+#include "zzlab.h"
+#include "zzlab/av.h"
+#include "zzlab/utils.h"
+#include "zzlab/gfx.h"
+#include "zzlab/net.h"
+#include "zzlab/wx.h"
+#include "zzlab/d3d9.h"
+#include "zzlab/di8.h"
+
+#include <comdef.h>
+#include <dinput.h>
+
+#include <boost/array.hpp>
+
+using namespace boost;
+using namespace zzlab;
+
+ZZLAB_USE_LOG4CPLUS(Init);
+
+extern utils::Timer* _Timer;
+extern d3d9::IDirect3D9ExPtr _d3d9ex;
+extern di8::IDirectInput8Ptr _di8;
+
+utils::Timer* oldTimer;
+
+d3d9::RenderWindow* rw;
+di8::Input* input = nullptr;
+
+utils::Delegate0 frameBeginDelegate;
+utils::Delegate0 drawDelegate;
+
+gfx::Camera camera;
+
+d3d9::EffectResource* effect;
+d3d9::MeshResource* mesh;
+d3d9::TextureResource* texture;
+D3DXCOLOR diffuse;
+
+void frameBegin()
+{
+	//ZZLAB_TRACE_FUNCTION();
+
+	input->update();
+
+	if (input->getKey(DIK_ESCAPE))
+	{
+		frameBeginDelegate.cancel();
+		drawDelegate.cancel();
+		rw->cancel();
+
+		_MainService.post(bind(&d3d9::RenderWindow::Destroy, rw));
+		return;
+	}
+
+	rw->waitForFrameBegin(frameBeginDelegate());
+}
+
+void draw()
+{
+	//ZZLAB_TRACE_FUNCTION();
+
+	effect->effect->SetTexture("MainTex", texture->textures[0]);
+	effect->effect->SetFloatArray("Diffuse", diffuse, sizeof(diffuse));
+
+	{
+		Eigen::Projective3f MATRIX_MVP = camera.matrix() * Eigen::Affine3f::Identity();
+		effect->effect->SetMatrix("MATRIX_MVP", (const D3DXMATRIX *)MATRIX_MVP.matrix().data());
+		mesh->draw(effect->effect);
+	}
+
+	rw->waitForFrameBegin(drawDelegate());
+}
+
+void initRenderer()
+{
+	ZZLAB_TRACE_FUNCTION();
+
+	rw->clear.color = D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f);
+	rw->init();
+
+	input = new di8::Input();
+	input->dinput = _di8;
+	input->hwnd = (HWND)rw->GetHandle();
+	input->init();
+
+	camera.proj = gfx::orthoLH(1, 1, 1.0f, 100.0f);
+	camera.updateT(
+		gfx::lookAtLH(
+		Eigen::Vector3f(0, 0, -10),
+		Eigen::Vector3f(0.0f, 0.0f, 0.0f),
+		Eigen::Vector3f::UnitY()));
+
+	effect = rw->get<d3d9::EffectResource>(L"Unlit.fx");
+	mesh = rw->get<d3d9::QuadMeshResource>(L"Quad");
+	texture = rw->get<d3d9::TextureResource>(L"aa.png");
+	diffuse = D3DXCOLOR(1, 1, 1, 1);
+
+	frameBeginDelegate.connect(frameBegin);
+	drawDelegate.connect(draw);
+
+	rw->waitForFrameBegin(frameBeginDelegate());
+	rw->waitForDraw(drawDelegate());
+}
+
+void init()
+{
+	ZZLAB_TRACE_FUNCTION();
+
+	oldTimer = _Timer;
+	_Timer = new utils::ManualTimer();
+
+	rw = d3d9::loadWindow(_d3d9ex, _Settings.first_node(L"D3D9Window1"));
+	gfx::_RenderService->post(bind(initRenderer));
+}
+
+void uninit()
+{
+	delete oldTimer;
+
+	ZZLAB_TRACE_FUNCTION();
+}
+
---
+++ wxTemplate/Main.cpp
@@ -0,0 +1,80 @@
+// wxTemplate.cpp : 定義應用程式的進入點。
+//
+
+#include "stdafx.h"
+
+#include "Resource.h"
+
+#include "zzlab.h"
+#include "zzlab/av.h"
+#include "zzlab/utils.h"
+#include "zzlab/gfx.h"
+#include "zzlab/net.h"
+#include "zzlab/wx.h"
+#include "zzlab/d3d9.h"
+#include "zzlab/di8.h"
+
+using namespace boost;
+using namespace zzlab;
+
+ZZLAB_USE_LOG4CPLUS(Main);
+
+utils::Timer* _Timer = nullptr;
+d3d9::IDirect3D9ExPtr _d3d9ex;
+di8::IDirectInput8Ptr _di8;
+
+extern void init();
+extern void uninit();
+
+class MyApp : public wx::App
+{
+public:
+	virtual bool OnInit();
+	virtual int OnExit();
+};
+
+bool MyApp::OnInit()
+{
+	// set log level
+	_LogFlags = LOG_TO_WIN32DEBUG | LOG_TO_FILE;
+	log4cplus::Logger::getRoot().setLogLevel(log4cplus::TRACE_LOG_LEVEL);
+
+	// install plugins
+	utils::install();
+	av::install();
+	wx::install();
+	gfx::install();
+	d3d9::install();
+
+	if (!wx::App::OnInit())
+		return false;
+
+	ZZLAB_TRACE_THIS();
+
+	_Timer = new utils::HiPerfTimer();
+	_d3d9ex = d3d9::createEx();
+	_di8 = di8::create();
+
+	zzlab::startWorkerService();
+	gfx::startRenderService();
+
+	init();
+
+	return true;
+}
+
+int MyApp::OnExit()
+{
+	uninit();
+
+	gfx::stopRenderService();
+	zzlab::stopWorkerService();
+	_d3d9ex = NULL;
+	_di8 = nullptr;
+	delete _Timer;
+	_Timer = nullptr;
+
+	return wx::App::OnExit();
+}
+
+wxIMPLEMENT_APP(MyApp);
